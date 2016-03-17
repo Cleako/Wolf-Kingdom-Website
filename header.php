4bdd8b1afa8a6e4fab7e8a99b0f212061baa0ce2
Brett Ellis <breellis@cisco.com>
2016-03-17 11:57:14 -0400

    Added Discord widget to the right.

--- header.php
+++ header.php
@@ -76,7 +76,7 @@ require_once './inc/charfunctions.php';
 						<span class="welcome-text">
 							<a href='/<?php echo $script_directory; ?>board/ucp.php?mode=logout&amp;sid=<?php print $user->data['session_id'];?>'>Log out</a>
 						</span>
-						
+						<iframe src="https://discordapp.com/widget?id=160002939178582016&theme=dark" width="320" height="400" allowtransparency="true" frameborder="0"></iframe>
 					<?php 
                                         } else { 
                                         ?>
@@ -94,6 +94,7 @@ require_once './inc/charfunctions.php';
 								</form> 
 								<a class="submit" href="/<?php echo $script_directory; ?>board/ucp.php?mode=register">Register</a> 
                                                             </div>
+                                                            <iframe src="https://discordapp.com/widget?id=160002939178582016&theme=dark" width="320" height="400" allowtransparency="true" frameborder="0"></iframe>
                                                     </div>
                                                 </aside
                                         <?php         
